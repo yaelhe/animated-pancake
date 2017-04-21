4978682be5d00c20bcb6fff86b56600ee5b36fb5
Ronen Amiel <ronena@wix.com>
2017-04-21 20:21:07 +0300

    Support for tests and overrides (#2)
    * fix linting, make tests run in node version > 7.6
    
    * somewhat large PR: support for overrides and full e2e tests (execpt for external commands)
    
    * fixing stuff with pr

--- index.js
+++ index.js
@@ -3,17 +3,34 @@
 'use strict';
 
 const program = require('commander');
+const chalk = require('chalk');
 const networksetup = require('./lib/networksetup');
 const startServer = require('./lib/start-server');
 
 program
   .command('start')
+  .option('-o, --override <from> <to>')
   .description('start a proxy server and listen to all outgoing http requests')
   .action(async () => {
+    const args = program.rawArgs;
+
+    const parseOptions = (acc, value, index) =>
+      value === '--override' ? Object.assign(acc, { [args[index + 1]]: args[index + 2] }) : acc;
+
+    const overrides = args.reduce(parseOptions, {});
+
     const [cleanupNetworksetup] = await Promise.all([
       networksetup(),
-      startServer(),
+      startServer(overrides),
     ]);
+
+    console.log(`${chalk.green('Woo-hoo! Chibi Chibi is up and running at http://localhost:7373!')}`);
+    console.log();
+    console.log(`To override this or other settings, check the ${chalk.cyan('~/.chibichibirc')} file`);
+    console.log(`Use the ${chalk.cyan('--help')} option for a list of configuration options`);
+    console.log();
+    console.log('Chibi Chibi will list proxied requests bellow');
+    console.log();
 
     process.on('SIGINT', async () => {
       await cleanupNetworksetup();
--- test/e2e/start.spec.js
+++ test/e2e/start.spec.js
@@ -1,11 +1,12 @@
 'use strict';
 
-const fs = require('fs');
-const {exec} = require('child_process');
-const mockBin = require('mock-bin');
-const {expect} = require('chai');
+const { exec } = require('child_process');
 const express = require('express');
+const { expect } = require('chai');
+const mockBin = require('mock-bin');
 const eventually = require('wix-eventually');
+const request = require('request-promise');
+const tempWrite = require('temp-write');
 
 const networksetupDevice = 'en0';
 const networkservicePort = 'Wi-Fi';
@@ -24,12 +25,14 @@ An asterisk (*) denotes that a network service is disabled.
 
 const ifconfigOutput = `
 en0: flags=8863<UP,BROADCAST,SMART,RUNNING,SIMPLEX,MULTICAST> mtu 1500
-	ether 60:f8:1d:c1:97:4e
-	inet6 fe80::62f8:1dff:fec1:974e%en0 prefixlen 64 scopeid 0x4
-	inet 10.0.0.2 netmask 0xffffff00 broadcast 10.0.0.255
-	nd6 options=1<PERFORMNUD>
-	media: autoselect
-	status: active`;
+  ether 60:f8:1d:c1:97:4e
+  inet6 fe80::62f8:1dff:fec1:974e%en0 prefixlen 64 scopeid 0x4
+  inet 10.0.0.2 netmask 0xffffff00 broadcast 10.0.0.255
+  nd6 options=1<PERFORMNUD>
+  media: autoselect
+  status: active`;
+
+const e2eServerPort = 4300;
 
 describe('start', () => {
   let proc;
@@ -38,6 +41,24 @@ describe('start', () => {
   let networksetupInitCalled;
   let networksetupCleanCalled;
   let networksetupOffCalled;
+  let serverRedirectCalled;
+
+  before(() => {
+    express()
+      .get('/init', (req, res) => {
+        networksetupInitCalled = true;
+        res.sendStatus(200);
+      })
+      .get('/clean', (req, res) => {
+        networksetupCleanCalled = true;
+        res.sendStatus(200);
+      })
+      .get('/off', (req, res) => {
+        networksetupOffCalled = true;
+        res.sendStatus(200);
+      })
+      .listen(e2eServerPort);
+  });
 
   beforeEach(() => {
     proc = null;
@@ -46,45 +67,17 @@ describe('start', () => {
     networksetupInitCalled = false;
     networksetupCleanCalled = false;
     networksetupOffCalled = false;
+    serverRedirectCalled = false;
   });
 
-  afterEach(() => {
-    proc.kill('SIGINT');
-  });
-
-  afterEach(async () => {
-    networksetupCleanup();
-    ifconfigCleanup();
-  });
-
-  const e2eServerPort = 4300;
-
-  before(done => {
-    const app = express()
-      .get('/init', (req, res) => {
-        networksetupInitCalled = true;
-        res.send('');
-      })
-      .get('/clean', (req, res) => {
-        networksetupCleanCalled = true;
-        res.send('');
-      })
-      .get('/off', (req, res) => {
-        networksetupOffCalled = true;
-        res.send('');
-      })
-      .listen(e2eServerPort, done);
-  });
-
-  it('should override network settings with web proxy settings and cleanup after termination', async () => {
+  beforeEach(async () => {
     const domain = 'localhost';
     const portnumber = '7373';
 
     networksetupCleanup = await mockBin(
       'networksetup',
       'node',
-      `const fs = require('fs');
-      const http = require('http');
+      `const http = require('http');
 
       if (process.argv[2] === '-listnetworkserviceorder') {
         console.log(\`${networksetupOutput}\`);
@@ -102,8 +95,21 @@ describe('start', () => {
       'node',
       `if (process.argv[2] === '${networksetupDevice}') { console.log(\`${ifconfigOutput}\`) }`
     );
+  });
 
-    proc = exec('node ./index.js start');
+  afterEach(() => {
+    if (proc) {
+      proc.kill('SIGINT');
+    }
+  });
+
+  afterEach(() => {
+    networksetupCleanup();
+    ifconfigCleanup();
+  });
+
+  it('should override network settings with web proxy settings and cleanup after termination', async () => {
+    proc = exec('./index.js start');
 
     await eventually(() => {
       expect(networksetupInitCalled).to.equal(true);
@@ -115,5 +121,48 @@ describe('start', () => {
       expect(networksetupCleanCalled).to.equal(true);
       expect(networksetupOffCalled).to.equal(true);
     });
+  });
+
+  it('proxy requests that do not match any overrides', async () => {
+    proc = exec('./index.js start');
+
+    await eventually(() => {
+      expect(networksetupInitCalled).to.equal(true);
+    });
+
+    express()
+      .use((req, res) => {
+        serverRedirectCalled = true;
+        res.send('');
+      })
+      .listen(9000);
+
+    await request({
+      url: 'http://localhost:7373',
+      headers: {
+        Host: 'localhost:9000'
+      }
+    });
+
+    expect(serverRedirectCalled).to.eql(true);
+  });
+
+  it('should serve files that match the --override path', async () => {
+    const filepath = await tempWrite('hello world', 'myfile');
+
+    proc = exec(`./index.js start --override http://wix.com/ ${filepath}`);
+
+    await eventually(() => {
+      expect(networksetupInitCalled).to.equal(true);
+    });
+
+    const response = await request({
+      url: 'http://localhost:7373',
+      headers: {
+        Host: 'wix.com'
+      }
+    });
+
+    expect(response).to.eql('hello world');
   });
 });
